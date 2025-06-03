cbe15a8b57c0f3e12473622efb325a45ccc193a9
D.N.SATHVARA <sathvaradhruvin05@gmail.com>
2025-06-03 16:38:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,12 @@
-const mongoose = require( "mongoose" );
+const mongoose = require("mongoose");
 
-const connectDB = async () =>
-{
-  try
-  {
-    await mongoose.connect( "mongodb://localhost:27017/form", {
-       useNewUrlParser: true,
-  useUnifiedTopology: true
-    } );
-    console.log( "MongoDB connected" );
-  } catch ( error )
-  {
-    console.error( "MongoDB connection failed:", error.message );
-    process.exit( 1 );
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/form");
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 };
 
